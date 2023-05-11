4aa718e921fcc881d4736070a115d73ed77a8d93
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-05-11 15:13:58 -0400

    Setup versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="AvidaScripts",
-    version="0.0",
+    version="0.0.0",
     packages=["AvidaScripts"],
     package_dir={'': '.'},
     install_requires=requirements,
