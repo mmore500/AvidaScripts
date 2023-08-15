2f6b5e626737d832918e3fbb3f24ab6a3e266f6e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-15 11:52:10 -0400

    Buff GenomeManipulation tests, move to tests/

---
+++ tests/test_GenomeManipulation/test_GenomeManipulator.py
@@ -0,0 +1,22 @@
+from AvidaScripts.GenericScripts.GenomeManipulation import (
+    GenomeManipulator,
+    make_named_instset_path,
+)
+
+
+def test_GenomeManipulator():
+    instset_path = make_named_instset_path("transsmt")
+    manipulator = GenomeManipulator(instset_path)
+    assert manipulator.sequence_to_genome("ab") == ["Nop-A", "Nop-B"]
+
+    all_point_mutants = manipulator.generate_all_point_mutants("ab")
+    assert all(len(seq) == 2 for seq in all_point_mutants)
+    assert "ab" not in all_point_mutants
+    assert len(all_point_mutants) >= len("ab")
+
+    all_mutants = manipulator.generate_all_mutants("ab")
+    assert "ab" not in all_mutants
+    assert not all(len(seq) == 2 for seq in all_mutants)
+    assert all(1 <= len(seq) <= 3 for seq in all_mutants)
+    assert len(all_mutants) > len("ab")
+    assert set(all_mutants) > set(all_point_mutants)
