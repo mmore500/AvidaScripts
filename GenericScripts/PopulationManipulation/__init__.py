480a4bcc978b2402e57c6dbff0b0223c90b074ff
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-15 11:26:15 -0400

    Add __all__ for PopulationManipulation module

--- GenericScripts/PopulationManipulation/__init__.py
+++ GenericScripts/PopulationManipulation/__init__.py
@@ -1,2 +1,7 @@
 from .extract_dominant_taxon import extract_dominant_taxon
 from .load_population_dataframe import load_population_dataframe
+
+__all__ = [
+    "extract_dominant_taxon",
+    "load_population_dataframe",
+]
