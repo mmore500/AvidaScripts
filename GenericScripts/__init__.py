62755af9659f7b4225af7b415c516868a77508ee
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-16 08:39:10 -0400

    Import top level modules in package init

--- GenericScripts/__init__.py
+++ GenericScripts/__init__.py
@@ -1,10 +1,16 @@
-from . import GenomeManipulation
-from . import PopulationManipulation
+from . import (
+    GenomeManipulation,
+    MutationalNeighborhood,
+    PhenotypeAssessment,
+    PopulationManipulation,
+)
 from .get_avida import get_avida_executable_path
 
 
 __all__ = [
     "GenomeManipulation",
+    "MutationalNeighborhood",
+    "PhenotypeAssessment",
     "PopulationManipulation",
     "get_avida_executable_path",
 ]
