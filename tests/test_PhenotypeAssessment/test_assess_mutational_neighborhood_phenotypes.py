94ff75b593bc18eb313756f495941e451be1364f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-16 20:39:19 -0400

    Test assessing phenotype over twostep neighborhood

--- tests/test_PhenotypeAssessment/test_assess_mutational_neighborhood_phenotypes.py
+++ tests/test_PhenotypeAssessment/test_assess_mutational_neighborhood_phenotypes.py
@@ -13,6 +13,7 @@ from AvidaScripts.GenericScripts.PhenotypeAssessment import (
 )
 from AvidaScripts.GenericScripts.MutationalNeighborhood import (
     get_onestep_pointmut_neighborhood,
+    get_twostep_pointmut_neighborhood,
 )
 
 
@@ -33,6 +34,32 @@ def test_get_onestep_pointmut_neighborhood(sequence: str):
     if sequence is not None:
         assert sequence in neighborhood
     assert all(0 <= step <= 1 for step in neighborhood.values())
+
+    for __, row in phenotypes_df.iterrows():
+        neighbor = row["Genome Sequence"]
+        step = row["Mutational Distance"]
+        assert sum(a != b for a, b in zip(sequence, neighbor)) == step
+
+    assert len(phenotypes_df) == len(neighborhood)
+
+
+@pytest.mark.parametrize("sequence", ["", "a", "b", "aa", "abab", None])
+def test_get_twostep_pointmut_neighborhood(sequence: str):
+
+    manipulator = GenomeManipulator(make_named_instset_path("transsmt"))
+
+    if sequence is not None:
+        neighborhood = get_twostep_pointmut_neighborhood(sequence, manipulator)
+    else:
+        neighborhood = dict()
+    phenotypes_df = assess_mutational_neighborhood_phenotypes(
+        neighborhood,
+        get_named_environment_content("top25"),
+        get_named_instset_content("transsmt"),
+    )
+    if sequence is not None:
+        assert sequence in neighborhood
+    assert all(0 <= step <= 2 for step in neighborhood.values())
 
     for __, row in phenotypes_df.iterrows():
         neighbor = row["Genome Sequence"]
