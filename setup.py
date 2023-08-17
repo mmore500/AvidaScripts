3db1c3f842be20861974fa9071d0908cc707edbd
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-17 10:36:34 -0400

    Format setup.py to black style

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="AvidaScripts",
     version="0.2.1",
     packages=find_packages(),
-    package_dir={'': '.'},
+    package_dir={"": "."},
     install_requires=requirements,
     author="Matthew Andres Moreno",
     author_email="morenoma@umich.edu",
     description="Repack of Luis Zaman's AvidaScripts as an installable package.",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/mmore500/AvidaScripts",
     classifiers=[
@@ -20,5 +20,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
