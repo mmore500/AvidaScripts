793032e89f0c6477f945894819fb1116a3977efe
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-17 09:31:05 -0400

    Remove unused import

--- GenericScripts/PhenotypeAssessment/count_environment_tasks.py
+++ GenericScripts/PhenotypeAssessment/count_environment_tasks.py
@@ -1,6 +1,3 @@
-from .get_named_environment_content import get_named_environment_content
-
-
 def count_environment_tasks(environment_content: str) -> str:
     """Count number of tasks defined in environment configuration."""
     return environment_content.count("REACTION ")
