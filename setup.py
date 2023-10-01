585c1f5efac72adb1a7048d5a9e7751c6ff73949
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-10-01 00:48:50 -0400

    Bump version: 0.7.4 → 0.7.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="AvidaScripts",
-    version="0.7.4",
+    version="0.7.5",
     packages=find_packages(),
     package_dir={"": "."},
     install_requires=requirements,
